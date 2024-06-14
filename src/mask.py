f4518cbf05ba89ad860b22aa3e6bf7f88a1b3991
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-06-14 10:02:39 +0700

    update gitignore

---
+++ src/mask.py
@@ -0,0 +1,14 @@
+def get_mask_card_number(number_cart: str) -> str:
+    """Функция,маскирующая номер карты клиента"""
+    return f"{number_cart[0:4]} {number_cart[4:6]}{"**  ****"} {number_cart[-4:]}"
+
+
+print(get_mask_card_number(str(7000792289606361)))
+
+
+def get_mask_account(number_account: str) -> str:
+    """Функция, маскирующая номер счета клиента"""
+    return f"{"**"}{number_account[-4:]}"
+
+
+print(get_mask_account(str(73654108430135874305)))
