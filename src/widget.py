5a74f434f22109f7a3dd124b1026b8ce255e7bfe
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-06-21 16:07:29 +0700

    доработка ДЗ аннотации типов

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,17 @@
-import mask
-
 from datetime import datetime
-
 from typing import Any
+
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number_account_card: str) -> Any:
     """Функция маскирующая счета и карты"""
     if "Счет " in number_account_card:
         number_accound = number_account_card[-20:]
-        return number_accound + mask.get_mask_account(number_accound)
+        return number_accound + get_mask_account(number_accound)
     else:
         number_card = "".join(number_account_card[-16:].split())
-        return number_account_card[:-16] + mask.get_mask_card_number(number_card)
+        return number_account_card[:-16] + get_mask_card_number(number_card)
 
 
 print(mask_account_card("Maestro 1596837868705199"))
