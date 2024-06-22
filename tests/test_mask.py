f1bb0c410d537285c964937798711be817c56092
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-06-22 23:18:40 +0700

    доработан файл test_mask b README

--- tests/test_mask.py
+++ tests/test_mask.py
@@ -2,7 +2,7 @@ from src.masks import get_mask_card_number, get_mask_account
 
 
 def test_get_mask_card_number():
-    assert get_mask_card_number('7000792289606361') == '7000 79 ** **** 6361'
+    assert get_mask_card_number('7000792289606361') == '7000 79** **** 6361'
 
 
 def test_get_mask_account():
