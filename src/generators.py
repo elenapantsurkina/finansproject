2a11d79f34260522a7f83a1185a647223f729f54
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-06-25 18:57:08 +0700

    создан модуль generators

---
+++ src/generators.py
@@ -0,0 +1,25 @@
+from typing import Generator
+
+
+def transaction_descriptions(transactions: list) -> Generator:
+    """Генератор, который принимает список словарей и возвращает описание каждой операции по очереди"""
+    for transaction in transactions:
+        yield transaction['description']
+
+
+def card_number_generator(start, end) -> Generator:
+    """Генератор номеров банковских карт"""
+    for number in range(start, end):
+        card_number = str(number)
+        while len(card_number) < 16:
+            card_number = '0' + card_number
+        formatted_cart_number = f"{card_number[0:5]} {card_number[5:9]} {card_number[9:13]} {-4:}"
+        yield formatted_cart_number
+
+
+def filter_by_currency(transactions: list, currency: str) -> Generator:
+    """Функция, которая принимает список словарей с банковскими операциями и возвращает итератор,
+    который выдает по очереди операции, в которых указана заданная валюта"""
+    for transaction in transactions:
+        if transaction['operationAmount']['currency']['code'] == currency:
+            yield transaction
