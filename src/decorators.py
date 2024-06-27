fdc8ddd2fb9e182de2d6eb7ae905c01331359d0f
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-06-27 22:55:42 +0700

    доработан модуль decorator

--- src/decorators.py
+++ src/decorators.py
@@ -5,23 +5,22 @@ from functools import wraps
 
 def log(filename: Any = None) -> Callable:
     """декоратор,который логирует вызов функции и ее результат в файл или в консоль"""
+
     def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args, **kwargs):
-            result = func(*args, **kwargs)
             try:
-                if filename:
-                    with open(filename, "a", encoding="utf-8") as file:
-                        file.write(f"my_function ok")
-                else:
-                    print(f"my_function ok")
+                result = func(*args, **kwargs)
+                log_messege = "my_function ok\n"
+
             except Exception as e:
-                if filename:
-                    with open(filename, "a", encoding="utf-8") as file:
-                        file.write(f"my_function error: {e}. Inputs: {args}, {kwargs} \n")
-                else:
-                    print(f"my_function error: {e}. Inputs: {args}, {kwargs} \n")
-                    raise
+                result = None
+                log_messege = f"my_function error: {e}. Inputs: {args}, {kwargs} \n"
+            if filename:
+                with open(filename, "a", encoding="utf-8") as file:
+                    file.write(log_messege)
+            else:
+                print(log_messege)
             return result
 
         return wrapper
@@ -29,9 +28,9 @@ def log(filename: Any = None) -> Callable:
     return decorator
 
 
-@log(filename="mylog.txt")
+@log()
 def my_function(x, y):
-    return x/y
+    return x / y
 
 
-print(my_function(2, 1))
+my_function(4, 0)
