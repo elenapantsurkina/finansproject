a5af7cf93377c65768227a2d0b62db07c790cdfb
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-07-09 22:13:48 +0700

    добавлены логеры в модули masks и utils

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,17 @@
 import logging
 
 
-logger = logging.getLogger('logs')
+logger = logging.getLogger("logs")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("..\\logs\\masks.log",encoding="utf-8")
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("..\\logs\\masks.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
 def get_mask_card_number(number_cart: str) -> str:
     """Функция,маскирующая номер карты клиента"""
-    logger.info(f'Маскируем карту клиента')
+    logger.info("Маскируем карту клиента")
     return f"{number_cart[0:4]} {number_cart[4:6]}{'** ****'} {number_cart[-4:]}"
 
 
@@ -20,7 +20,7 @@ print(get_mask_card_number(str(7000792289606361)))
 
 def get_mask_account(number_account: str) -> str:
     """Функция, маскирующая номер счета клиента"""
-    logger.info(f'Маскируем счет клиента')
+    logger.info("Маскируем счет клиента")
     return f"{'**'}{number_account[-4:]}"
 
 
--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,10 @@ import json
 import logging
 
 
-logger = logging.getLogger('logs')
+logger = logging.getLogger("logs")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler("..\\logs\\utils.log",encoding="utf-8")
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler = logging.FileHandler("..\\logs\\utils.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
@@ -13,19 +13,19 @@ logger.addHandler(file_handler)
 def get_transactions_list(my_file_path: str) -> list:
     """Функция принимающая путь до файла в формате json и возвращающая список словарей"""
     try:
-        logger.info(f'Указываем путь до файла')
-        with (open(my_file_path, "r", encoding="utf-8") as f):
+        logger.info("Указываем путь до файла")
+        with open(my_file_path, "r", encoding="utf-8") as f:
             try:
-                logger.info(f'Получаем список транзакций')
+                logger.info("Получаем список транзакций")
                 transactions_list_my = json.load(f)
                 if transactions_list_my == []:
                     return []
             except json.JSONDecodeError as ex:
-                logger.error(f'Произошла ошибка: {ex}')
+                logger.error(f"Произошла ошибка: {ex}")
 
                 return []
     except FileNotFoundError as ex:
-        logger.error(f'Произошла ошибка: {ex}')
+        logger.error(f"Произошла ошибка: {ex}")
         return []
 
     return transactions_list_my
