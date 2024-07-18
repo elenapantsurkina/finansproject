d95983b035048be2609fb1a2de961d5b4f1ebff2
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-07-18 19:31:37 +0700

    доработан модуль main

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
-import json
+
 from src.utils import get_transactions_list
 from src.reader_file import reader_file_transaction_csv, reader_file_transaction_excel
 from src.processing import filter_by_state, sort_by_date
 from src.filter_transactions import get_filter_transaction
 from src.filter_transactions_category import get_filter_transaction_category
-from src.external_api import get_transaction_sum_rub
+
 from src.widget import mask_account_card, get_data
 
 
 def main():
     while True:
-        user_input = input("Привет! Добро пожаловать в программу работы с банковскими транзакциями.\n"
-                           "Выберите необходимый пункт меню:\n"
-                           "1. Получить информацию о транзакциях из JSON-файла\n"
-                           "2. Получить информацию о транзакциях из CSV-файла\n"
-                           "3. Получить информацию о транзакциях из XLSX-файла\n")
+        user_input = input(
+            "Привет! Добро пожаловать в программу работы с банковскими транзакциями.\n"
+            "Выберите необходимый пункт меню:\n"
+            "1. Получить информацию о транзакциях из JSON-файла\n"
+            "2. Получить информацию о транзакциях из CSV-файла\n"
+            "3. Получить информацию о транзакциях из XLSX-файла\n"
+        )
         if user_input not in ["1", "2", "3"]:
             print("Вами не выбран доступный формат файла")
             break
@@ -31,15 +33,16 @@ def main():
             # continue
             transactions = reader_file_transaction_excel("..\\data\\transactions_excel.xlsx")
 
-        input_user_state = input("Введите статус, по которому необходимо выполнить фильтрацию.\n "
-                                 "Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING\n").upper()
+        input_user_state = input(
+            "Введите статус, по которому необходимо выполнить фильтрацию.\n "
+            "Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING\n"
+        ).upper()
         if input_user_state not in ["EXECUTED", "CANCELED", "PENDING"]:
             print(f"Статус операции {input_user_state} не доступен.")
             break
         else:
             print(f"Операции отфильтрованы по статусу{input_user_state}")
             filtered_transactions = filter_by_state(transactions, input_user_state)
-
 
         user_input_data = input("Отсортировать операции по дате? Да/Нет\n").upper()
         if user_input_data == "ДА":
@@ -51,22 +54,28 @@ def main():
 
         input_user_rub = input("Выводить только рублевые тразакции? Да/Нет\n").upper()
         if input_user_rub == "ДА":
-            filtered_transactions = [t for t in filtered_transactions if t["operationAmount"]["currency"]["code"] == "RUB"]
+            filtered_transactions = [
+                t for t in filtered_transactions if t["operationAmount"]["currency"]["code"] == "RUB"
+            ]
 
         input_user_filter_description = input(
-                            "Отфильтровать список транзакций по определенному слову в описании? Да/Нет\n").upper()
+            "Отфильтровать список транзакций по определенному слову в описании? Да/Нет\n"
+        ).upper()
         if input_user_filter_description == "ДА":
             input_user_filter_description = input("Введите категорию фильтрации: ").capitalize()
-            filtered_transactions = get_filter_transaction(filtered_transactions,
-                                                                           bank_op=input_user_filter_description)
+            filtered_transactions = get_filter_transaction(
+                filtered_transactions, bank_op=input_user_filter_description
+            )
             print("Распечатываю итоговый список транзакций...")
             if filtered_transactions:
                 counter_category = get_filter_transaction_category(filtered_transactions)
-                print(f"Всего банковских операций в выборке: {input_user_filter_description}: {counter_category[input_user_filter_description]}")
-                #print(filtered_transactions)
+                print(
+                    f"Всего банковских операций в выборке: {input_user_filter_description}: {counter_category[input_user_filter_description]}"
+                )
+                # print(filtered_transactions)
                 for trans in filtered_transactions:
                     print(f"Дата: {get_data(trans['date'])}, {trans['description']}")
-                    if trans.get('from'):
+                    if trans.get("from"):
                         print(f"Счет: {mask_account_card(trans['from'])} -> Счет: {mask_account_card(trans['to'])}")
                     else:
                         print(f"Счет: {mask_account_card(trans['to'])}")
@@ -81,17 +90,15 @@ def main():
             print(f"Всего банковских операций в выборке: {len(filtered_transactions)}")
             for trans in filtered_transactions:
                 print(f"Дата: {get_data(trans['date'])}, {trans['description']}")
-                if trans.get('from'):
+                if trans.get("from"):
                     print(f"Счет: {mask_account_card(trans['from'])} -> Счет: {mask_account_card(trans['to'])}")
                 else:
                     print(f"Счет: {mask_account_card(trans['to'])}")
                 print(f"Сумма: {trans['operationAmount']['amount']}")
-            #print(filtered_transactions)
+            # print(filtered_transactions)
             break
 
     return
-
-
 
 
 if __name__ == "__main__":
--- src/masks.py
+++ src/masks.py
@@ -14,7 +14,7 @@ def get_mask_card_number(number_cart: str) -> str:
     return f"{number_cart[0:4]} {number_cart[4:6]}{'** ****'} {number_cart[-4:]}"
 
 
-#print(get_mask_card_number(str(7000792289606361)))
+# print(get_mask_card_number(str(7000792289606361)))
 
 
 def get_mask_account(number_account: str) -> str:
@@ -23,4 +23,4 @@ def get_mask_account(number_account: str) -> str:
     return f"{'**'}{number_account[-4:]}"
 
 
-#print(get_mask_account(str(73654108430135874305)))
+# print(get_mask_account(str(73654108430135874305)))
